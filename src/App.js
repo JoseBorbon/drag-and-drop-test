99909b7180a9c09539bc41702836d2ab53da8303
JoseBorbon <joserborbon@gmail.com>
2021-12-27 21:17:56 -0500

    base

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 function App() {
+  const data = [
+    { name: 'one', id: '1' },
+    { name: 'two', id: '2' },
+    { name: 'three', id: '3' },
+    { name: 'four', id: '4' },
+    { name: 'five', id: '5' },
+  ];
+
+  const [list, setList] = useState(data);
+
+  const onEnd = (result) => {
+    console.log(result);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DragDropContext onDragEnd={onEnd}>
+      <Droppable droppableId="testDragAndDrop">
+        {(provided, snapshot) => (
+          <div ref={provided.innerRef}>
+            {list.map((item, index) => (
+              <Draggable draggableId={item.id} key={item.id} index={index}>
+                {(provided, snapshot) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    <div>{item.name}</div>
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 }
 
